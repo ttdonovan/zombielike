26230a206a1522760d68a2e704ae2871c4dcb2a2
Tanner Donovan <ttdonovan@gmail.com>
2018-03-01 21:19:52 -0800

    CryptoZombies Lesson 2: Zombies Attack Their Victims

---
+++ cryptozombies/lesson_02/ZombieFactory.sol
@@ -0,0 +1,39 @@
+pragma solidity 0.4.20;
+
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint internal dnaDigits = 16;
+    uint internal dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) private ownerZombieCount;
+
+    function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+    function _createZombie(string _name, uint _dna) internal {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+}
